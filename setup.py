30462fca5b90da9d995fbe1f221369fc9f47cea1
eggplants <w10776e8w@yahoo.co.jp>
2021-08-28 20:41:35 +0900

    v0.3: update docs and some fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup  # type: ignore
 
 setup(
     name='gppt',
-    version="0.2",
+    version="0.3",
     description='',
     description_content_type='',
     long_description=open('README.md').read(),

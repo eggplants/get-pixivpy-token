279783258327d1bc36107d9022df11d14003edb4
eggplants <w10776e8w@yahoo.co.jp>
2022-12-23 15:28:42 +0900

    fix: remove useless lines

--- gppt/_selenium.py
+++ gppt/_selenium.py
@@ -184,8 +184,6 @@ class GetPixivToken:
             options.add_argument("--disable-infobars")
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--disable-browser-side-navigation")
-            options.add_argument('--proxy-server="direct://"')
-            options.add_argument("--proxy-bypass-list=*")
             options.add_argument("--start-maximized")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
